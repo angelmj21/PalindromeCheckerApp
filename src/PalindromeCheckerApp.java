c70b483df9eee2f2b3a6d2808b17b5847ce93315
angelmj21 <aj7762@srmist.edu.in>
2026-02-27 14:47:23 +0530

    UC2: Hardcoded Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-      String str ="madam";
-      if (str.equals(new StringBuilder(str).reverse().toString())){
-          System.out.println(str + " is palindrome");
+        String str = "madam";
 
-      }else{
-          System.out.println(str+" is NOT palindrome");
-
-      }
-    }}
+        if (str.equals(new StringBuilder(str).reverse().toString()))
+            System.out.println(str + " is palindrome");
+        else
+            System.out.println(str + " is NOT palindrome");
+    }
+}
