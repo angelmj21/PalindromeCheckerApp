edbdb2c94a27316f92cc262214f536426b699fdb
angelmj21 <aj7762@srmist.edu.in>
2026-02-27 14:49:52 +0530

    UC3: Reverse String Using Loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String str = "madam";
+        String str = "level";
+        String rev = "";
 
-        if (str.equals(new StringBuilder(str).reverse().toString()))
+        for (int i = str.length() - 1; i >= 0; i--)
+            rev += str.charAt(i);
+
+        if (str.equals(rev))
             System.out.println(str + " is palindrome");
         else
             System.out.println(str + " is NOT palindrome");
