ed2f15fb44a788e0d8ed5b2fbb0a78e9f6ebc175
angelmj21 <aj7762@srmist.edu.in>
2026-03-09 15:06:12 +0530

    UC12: Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,36 +1,73 @@
 import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-class PalindromeChecker {
+interface PalindromeStrategy {
+    boolean checkPalindrome(String str);
+}
 
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
     public boolean checkPalindrome(String str) {
         str = str.replaceAll("\\s+", "").toLowerCase();
 
         Stack<Character> stack = new Stack<>();
 
-        // Push characters to stack
-        for (int i = 0; i < str.length(); i++) {
-            stack.push(str.charAt(i));
+        for (char c : str.toCharArray()) {
+            stack.push(c);
         }
 
-        // Compare while popping
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != stack.pop()) {
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
         }
-
         return true;
     }
 }
 
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : str.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Service class
+class PalindromeService {
+    private PalindromeStrategy strategy;
+
+    public PalindromeService(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String str) {
+        return strategy.checkPalindrome(str);
+    }
+}
+
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-        String input = "Madam";
+        String input = "mada";
 
-        PalindromeChecker checker = new PalindromeChecker();
+        // Choose strategy dynamically
+        PalindromeService service = new PalindromeService(new StackStrategy());
+        // PalindromeService service = new PalindromeService(new DequeStrategy());
 
-        if (checker.checkPalindrome(input))
+        if (service.check(input))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
